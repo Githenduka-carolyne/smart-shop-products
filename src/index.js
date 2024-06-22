15bc6e7975111791e2c9420245f9ceac0339b240
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-06-22 23:56:58 +0300

    add the patch and post code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import productsrouters from "./routes/products.routes.js";
 const app = express();
-
-app.use("/products", productsrouters);
 app.use(express.json());
+app.use("/products", productsrouters);
 
 
-app.get("/",(req, res) =>{
+
+app.get("/products",(req, res) =>{
  res.send("you have hit the product endpoint")
 })
 
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -27,49 +27,96 @@ router.get ("/:id", async (req, res)=>{
     // res.send("getting a single products")
 })
 
-router.post ("/",async(req, res)=>{
-     try{
-        const productthumbnail = req.body.productthumbnail;
-        const producttitle = req.body.producttitle;
-        const productdescription = req.body.productdescription;
-        const productcost = req.body.productcost;
-        const onoffer = req.body.onoffer;
+router.post("/", async (req, res) => {
+  const productthumbnail = req.body.productthumbnail;
+  const producttitle = req.body.producttitle;
+  const productdescription = req.body.productdescription;
+  const productcost = req.body.productcost;
+  const onoffer = req.body.onoffer;
+  if (!productthumbnail)
+    return res
+      .status(400)
+      .json({ success: false, message: "Product thumbnail field is required" });
+  if (!producttitle)
+    return res
+      .status(400)
+      .json({ success: false, message: "Product title field is required" });
+  if (!productdescription)
+    return res.status(400).json({
+      success: false,
+      message: "Product description field is required",
+    });
+  if (!productcost)
+    return res
+      .status(400)
+      .json({ success: false, message: "Product cost field is required" });
+  if (!onoffer)
+    return res
+      .status(400)
+      .json({ success: false, message: "Onoffer field is required" });
+  try {
+    const result = await pool.query(
+      "INSERT INTO products (productThumbnail, productTitle, productDescription, productCost, onOffer) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [
+        productthumbnail,
+        producttitle,
+        productdescription,
+        productcost,
+        onoffer,
+      ],
+    );
+    res.status(201).json({ success: true, data: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+router.patch("/:id", async (req, res) => {
+  const {
+    productthumbnail,
+    producttitle,
+    productdescription,
+    productcost,
+    onoffer,
+  } = req.body;
+  const id = req.params.id;
 
-   const insert = await pool.query ("INSERT INTO products(productthumbnail , producttitle, productdescription, productcost, onoffer) VALUES($1, $2, $3, $4, $5) RETURNING *",
-    [productthumbnail ,producttitle, productdescription, productcost, onoffer ]);
-        if (insert.rowCount === 1){
-        res.status(201).json({success:true,data: insert.rows[0]});
-        }
-    }catch(err){
-        res.status(500).json({success:false, message:err.message})
+  try {
+    let updateProduct;
+    if (productthumbnail) {
+      updateProduct = await pool.query(
+        "UPDATE Products SET productthumbnail=$1 WHERE id=$2",
+        [productthumbnail, id],
+      );
     }
-})
-
-router.patch ("/:id", async(req, res)=>{
-    const{productthumbnail, producttitle, productdescription, productcost, onoffer}=req.body;
-    try{
-        let updateOperation;
-        if(productthumbnail){updateOperation = await pool.query("UPDATE products SET productthumbnail = $1 WHERE id=$2", [productthumbnail, id]);     
-        }
-        if(producttitle){updateOperation = await pool.query("UPDATE products SET producttitle = $1 WHERE id=$2", [producttitle, id]);     
-        }
-        if(productdescription){updateOperation = await pool.query("UPDATE products SET productdescription = $1 WHERE id=$2", [productdescription, id]);     
-        }
-        if(productcost){updateOperation = await pool.query("UPDATE products SET productcost = $1 WHERE id=$2", [productcost, id]);     
-        }
-        if(onoffer){updateOperation = await pool.query("UPDATE products SET onoffer = $1 WHERE id=$2", [onoffer, id]);     
-        }
-        if (updateOperation.rowCount === 1){
-            res.status(200).json({success:true , message:"update done successfully"});
-        }else{
-            res.status(400).json({success: false, message:"Invalid user"})
-        }  
-    }catch{
-        res.status(500).json({success: false, message:err.message})
+    if (producttitle) {
+      updateProduct = await pool.query(
+        "UPDATE Products SET producttitle=$1 WHERE id=$2",
+        [producttitle, id],
+      );
     }
-    
-})
-
+    if (productdescription) {
+      updateProduct = await pool.query(
+        "UPDATE Products SET productdescription=$1 WHERE id=$2",
+        [productdescription, id],
+      );
+    }
+    if (productcost) {
+      updateProduct = await pool.query(
+        "UPDATE Products SET productcost=$1 WHERE id=$2",
+        [productcost, id],
+      );
+    }
+    if (onoffer) {
+      updateProduct = await pool.query(
+        "UPDATE Products SET onoffer=$1 WHERE id=$2",
+        [onoffer, id],
+      );
+    }
+    res.json(updateProduct);
+  } catch (error) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
 router.delete ("/:id", async (req, res)=>{
     const id = req.params.id;
     try{
